dd1caa9cde373bc401ae889e51516d5d5f37991d
Startrekz35 <burkz3905@gmail.com>
2022-08-08 22:15:38 -0500

    Project Beginning

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int add(int x, int y) //Addition
+{
+    return x + y;
+}
+
+void addandlog(int x, int y)
+{
+int result = add(x, y);
+std::cout << result << std::endl;
+}
+
+
+int subtraction(int x, int y)
+{
+    return x - y;
+}
+
+void subtractandlog(int x, int y)
+{
+int result = subtraction(x, y);
+std::cout << result << std::endl;
+}
+
+
+int multiply(int x, int y)
+{
+    return x * y;
+}
+
+void multiplyandlog(int x, int y)
+{
+int result = multiply(x, y);
+std::cout << result << std::endl;
+}
+
+int main()
+{
+
+multiplyandlog(5,4);
+
+
+
+    return 0;
+}
